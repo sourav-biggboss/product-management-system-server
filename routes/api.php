f282d5d4d7c4b9ea21891f1bb79d7e1baade9fa0
Biggboss <71979923+sourav-biggboss@users.noreply.github.com>
2023-01-04 20:29:24 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -17,44 +17,45 @@ use App\Http\Controllers\CommonApiController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-});
-
-Route::middleware('auth:api')->group(function (){
-
-    Route::prefix('profile')->name('profile.')->controller(ProfileController::class)->group(function (){
-        Route::get('edit/{id?}','edit')->whereNumber('id');
-        Route::post('create','create')->name('create');
-        Route::get('index/{id?}','index')->whereNumber('id')->name('index');
-        Route::put('update/{id?}','update')->whereNumber('id');
-        Route::delete('delete/{id?}','destroy')->whereNumber('id');
-        Route::get('fetch-user-with-department/{id}','fetchUserWithDepartment')->whereNumber('id');
-
+Route::middleware(['cors'])->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
     });
 
-    Route::prefix('department')->name('department.')->controller(DepartmentController::class)->group(function (){
-        Route::get('index/{id?}','index')->whereNumber('id')->name('index');
-        Route::post('create','create')->name('create');
-        Route::put('update/{id}','update')->whereNumber('id')->name('update');
-        Route::delete('delete/{id}','destroy')->whereNumber('id')->name('delete');
-    });
+    Route::middleware('auth:api')->group(function (){
 
-    Route::prefix('notification')->name('notification.')->controller(NotificationController::class)->group(function (){
-        Route::get('index/{id?}','index')->whereNumber('id')->name('index');
-        Route::post('create','create')->name('create');
-    });
+        Route::prefix('profile')->name('profile.')->controller(ProfileController::class)->group(function (){
+            Route::get('edit/{id?}','edit')->whereNumber('id');
+            Route::post('create','create')->name('create');
+            Route::get('index/{id?}','index')->whereNumber('id')->name('index');
+            Route::put('update/{id?}','update')->whereNumber('id');
+            Route::delete('delete/{id?}','destroy')->whereNumber('id');
+            Route::get('fetch-user-with-department/{id}','fetchUserWithDepartment')->whereNumber('id');
 
-    Route::prefix('common-api')->name('common-api.')->controller(CommonApiController::class)->group(function (){
-        Route::get('index/{model}/{offset?}','index')->whereAlpha('model')->whereNumber('offset')->name('index');
-        Route::get('count/{model}','count')->whereAlpha('model')->name('count');
-        Route::post('create/{model}','create')->whereAlpha('model')->name('create');
-        Route::put('update/{model}/{id}','update')->whereAlpha('model')->whereNumber('id')->name('update');
-        Route::delete('delete/{model}/{id}','destroy')->whereAlpha('model')->whereNumber('id')->name('delete');
-    });
+        });
 
+        Route::prefix('department')->name('department.')->controller(DepartmentController::class)->group(function (){
+            Route::get('index/{id?}','index')->whereNumber('id')->name('index');
+            Route::post('create','create')->name('create');
+            Route::put('update/{id}','update')->whereNumber('id')->name('update');
+            Route::delete('delete/{id}','destroy')->whereNumber('id')->name('delete');
+        });
+
+        Route::prefix('notification')->name('notification.')->controller(NotificationController::class)->group(function (){
+            Route::get('index/{id?}','index')->whereNumber('id')->name('index');
+            Route::post('create','create')->name('create');
+        });
+
+        Route::prefix('common-api')->name('common-api.')->controller(CommonApiController::class)->group(function (){
+            Route::get('index/{model}/{offset?}','index')->whereAlpha('model')->whereNumber('offset')->name('index');
+            Route::get('count/{model}','count')->whereAlpha('model')->name('count');
+            Route::post('create/{model}','create')->whereAlpha('model')->name('create');
+            Route::put('update/{model}/{id}','update')->whereAlpha('model')->whereNumber('id')->name('update');
+            Route::delete('delete/{model}/{id}','destroy')->whereAlpha('model')->whereNumber('id')->name('delete');
+        });
+
+    });
 });
